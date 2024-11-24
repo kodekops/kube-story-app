9d8558070c9c1d484d359ac9ea72ce0b4f3fb286
Khadga Shrestha <khadgalovecoding2016@gmail.com>
2024-11-24 09:30:43 +0545

    added env variables

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const filePath = path.join(__dirname, 'story', 'text.txt');
+const filePath = path.join(__dirname, process.env.STORY_DIR, 'text.txt');
 
 app.use(bodyParser.json());
 
